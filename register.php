6bba0cba1fecd1e38816f24b6f09ca2b63c55a07
phuongtraineeweb <76764159+nguyenthiphuong01@users.noreply.github.com>
2022-03-29 20:44:26 +0700

    Add files via upload

---
+++ register.php
@@ -0,0 +1,32 @@
+<?php include('server.php') ?>
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Web</title>
+  <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+  <div class="header">
+  	<h2>Register</h2>
+  </div>
+  <form method="post" action="register.php">
+  	<?php include('errors.php'); ?>
+  	<div class="input-group">
+  	  <label>Username</label>
+  	  <input type="text" name="username" value="<?php echo $username; ?>">
+  	</div>
+  	
+  	<div class="input-group">
+  	  <label>Password</label>
+  	  <input type="password" name="password">
+  	</div>
+
+  	<div class="input-group">
+  	  <button type="submit" class="btn" name="reg_user">Register</button>
+  	</div>
+  	<p>
+  		Already have an account? <a href="login.php">Login</a>
+  	</p>
+  </form>
+</body>
+</html>
---
+++ server.php
@@ -0,0 +1,61 @@
+<?php
+session_start();
+
+$username = "";
+$errors = array(); 
+
+$db = mysqli_connect('localhost', 'root', '', 'login_db');
+
+if (isset($_POST['reg_user'])) {
+  $username = mysqli_real_escape_string($db, $_POST['username']);
+  $password = mysqli_real_escape_string($db, $_POST['password']);
+
+  if (empty($username)) { array_push($errors, "Username is required"); }
+  if (empty($password)) { array_push($errors, "Password is required"); }
+
+  $user_check_query = "SELECT * FROM users WHERE username='$username' LIMIT 1";
+  $result = mysqli_query($db, $user_check_query);
+  $user = mysqli_fetch_assoc($result);
+  
+  if ($user) { 
+    if ($user['username'] === $username) {
+      array_push($errors, "Username already exists");
+    }
+  }
+
+  if (count($errors) == 0) {
+  	$password = md5($password);
+
+  	$query = "INSERT INTO users (username, password) 
+  			  VALUES('$username', '$password')";
+  	mysqli_query($db, $query);
+  	$_SESSION['username'] = $username;
+  	header('location: index.php');
+  }
+}
+
+if (isset($_POST['login_user'])) {
+  $username = mysqli_real_escape_string($db, $_POST['username']);
+  $password = mysqli_real_escape_string($db, $_POST['password']);
+
+  if (empty($username)) {
+  	array_push($errors, "Username is required");
+  }
+  if (empty($password)) {
+  	array_push($errors, "Password is required");
+  }
+
+  if (count($errors) == 0) {
+  	$password = md5($password);
+  	$query = "SELECT * FROM users WHERE username='$username' AND password='$password'";
+  	$results = mysqli_query($db, $query);
+  	if (mysqli_num_rows($results) == 1) {
+  	  $_SESSION['username'] = $username;
+  	  header('location: index.php');
+  	}else {
+  		array_push($errors, "Wrong username/password");
+  	}
+  }
+}
+
+?>
