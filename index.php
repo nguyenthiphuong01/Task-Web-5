7419d0a243f1c04c3e451e385160ee7638dafb02
phuongtraineeweb <76764159+nguyenthiphuong01@users.noreply.github.com>
2022-04-21 15:08:08 +0700

    Update index.php

--- index.php
+++ index.php
@@ -23,5 +23,24 @@
 			<p> <a href="login.php" style="color: #d6b0de;"> Here </a> </p>	
 		<?php endif ?>
 	</div>
+	
+	<div class="union">
+		<?php include("server.php");
+			if (isset($_GET['firstname']) && isset($_GET['lastname'])){
+				$firstname = $_GET['firstname'];
+				$lastname = $_GET['lastname'];
+				
+				$query = "SELECT * FROM names WHERE firstname='$firstname' AND lastname='$lastname'";
+				$datas = $db->query($query);
+				if ($datas->num_rows > 0) {
+					while($row = $datas->fetch_assoc()) {
+						echo "<br> ".$row["id"]. " ".$row["firstname"]. " ".$row["lastname"]. "<br>";
+					} 
+				} else {
+					echo "No result!";
+				} 
+			}			
+		?>
+	</div>
 </body>
 </html>
